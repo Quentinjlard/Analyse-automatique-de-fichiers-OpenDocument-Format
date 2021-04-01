d2f0056513e9458554e63a8af3a523d02434fdb4
Quentin_jlard <60688034+Quentinjlard@users.noreply.github.com>
2021-04-01 17:27:15 +0200

    Mise a jour du menu

--- menu.java
+++ menu.java
@@ -5,59 +5,71 @@ public class menu{
 	public static void main(String[]args){
 
         Scanner clavier = new Scanner (System.in);
+        int sousmenu = 0;
         boolean arret = false;
 		System.out.println("--------------------------------------------------------");
 		System.out.println("-----------------ANALYSEUR DE FICHIER-------------------");
 		System.out.println("--------------------------------------------------------");
 
-        while (!arret){
-			
-			System.out.println("-----------------------------------------");
-			System.out.println("---Quel Fichier voulez-vous analysez? ---");
-			System.out.println("------------Merci de choisir-------------");
-			System.out.println("-----------------------------------------");
-			System.out.println("META : tapez 1");
-			System.out.println("STYLES : tapez 2");
-			System.out.println("Content : tapez 2");
-			System.out.println("Quitter : tapez 9");
-
-			switch(clavier.nextInt()){
-				case 1 : {
-					System.out.println("Demarrage de l'analyse du META"); 
-					metaodt.main(fichier());
-					break;
-				}	
-				case 2 : {
-					System.out.println("Demarrage de l'analyse du STYLES"); 
-					break;
-				}
-				case 3 : {
-					System.out.println("Demarrage de l'analyse du Content"); 
-					break;
-				}
-				case 9 : {
-					arret = true; 
-					break;
-				}
-				default : {
-					System.out.println("entrez un choix entre 1 et 3"); 
-					break;
-				}
-			}
-		}
-	}
-
-	private static String[] fichier(){
-
-		Scanner clavier = new Scanner (System.in);
-		System.out.println("--------------------------------------------------------");
-		System.out.println("---Merci de saisir le nom du fichier avec l'extension---");
-		System.out.println("--------------------------------------------------------");
+		System.out.println("--Merci de saisir le nom du fichier avec l'extension--");
 		String fichier = clavier.nextLine();
-		String [] tab_fichier = new String[1];
 
+		String [] tab_fichier = new String[1];
+		
 		tab_fichier[0] = fichier;
 
-		return tab_fichier;
+        while (!arret){
+		System.out.println("--Type de Fichier a analyser--");
+		System.out.println(" odt  (Fichier TEXTE) : tapez 1");
+		System.out.println(" ods  (Fichier TABLEAU) : tapez 2");
+		System.out.println(" odp  (Fichier DIAPORAMA) : tapez 3");
+        System.out.println("quitter : tapez 9");
+		sousmenu = clavier.nextInt();
+		switch(sousmenu)
+		{
+			case 1 :{
+				int choix = 0;
+				System.out.println("---Quel Fichier voulez-vous analysez? ---");
+				System.out.println("Meta : Une liste de champs prédéfinis fait partie du standard : application, titre, description, sujet, mots-clés, auteur initial, auteur, imprimé par, date de création, date de dernière modification, date de dernière impression, durée d'édition, modèle utilisé, rechargement automatique, langue, nombre d'éditions, durée totale d'édition, statistiques sur le document. ");
+				System.out.println("Styles : Les styles s'appliquent sur toutes sortes d'objets : paragraphes, cadres, pages, caractères, numérotations, etc");
+				System.out.println("Content : il contient le contenu réel du document ");
+				System.out.println("--Merci de choisir--");
+				System.out.println("META : tapez 1");
+				System.out.println("STYLES : tapez 2");
+				System.out.println("Content : tapez 2");
+				System.out.println("quitter : tapez 9");
+				choix = clavier.nextInt();
+					switch(choix){
+						case 1 : {
+							TestZipStream.main(tab_fichier);
+							System.out.println("Demarrage de l'analyse du META"); 
+							break;
+						}	
+						case 2 : {
+							System.out.println("Demarrage de l'analyse du STYLES"); 
+							break;
+						}
+						case 3 : {
+							System.out.println("Demarrage de l'analyse du Content"); 
+							break;
+						}
+						case 9 : {
+							arret = true; 
+							break;
+						}
+						default : {
+							System.out.println("entrez un choix entre 1 et 3"); 
+							break;
+						}
+					}
+				} break;
+
+
+			case 2 : System.out.println("Analyse Tableau en cours de création"); break;
+			case 3 : System.out.println("Analyse Diaporama en couurs de création"); break;
+			case 9 : arret = true; break;
+			default : System.out.println("entrez un choix entre 1 et 3"); break;
+			}
+		}
 	}
 }
