351e7f7778cc36068e82af3f8aa53ad8ed0d388f
Tehcam <tehcam42@gmail.com>
2021-04-03 22:11:41 +0200

    Mise en place de l'arborescence

---
+++ prog/src/cls/odf/odt/test.java
@@ -0,0 +1,10 @@
+package cls.odf.odt;
+
+public class test
+{
+    public test()
+    {
+        // une petite classe pour tester l'utilisation du package !
+        System.out.println("hello world !");
+    }
+}
---
+++ prog/src/odfanalyser.java
@@ -0,0 +1,11 @@
+import cls.odf.odt.*;
+// import cls.odf.odp.*;
+// import cls.odf.ods.*;
+
+public class odfanalyser
+{
+    public static void main(String[] args)
+    {
+        new test();
+    }
+}
